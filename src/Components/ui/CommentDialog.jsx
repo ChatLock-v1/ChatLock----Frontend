a09127feaa8386d8d5bf13c6f1558d8ebd57629d
Coder Irfan <mohdirfan70097@gmail.com>
2025-07-22 01:10:43 +0530

    Comment Dialog Box

--- src/Components/ui/CommentDialog.jsx
+++ src/Components/ui/CommentDialog.jsx
@@ -41,7 +41,50 @@ function CommentDialog({ open, setOpen }) {
             objectFit: 'contain',
           }}
         />
-        <h1>baby</h1>
+       <div className='w-1/2 flex flex-col justify-between'>
+            <div className='flex items-center justify-between p-4'>
+              <div className='flex gap-3 items-center'>
+                <Link>
+                  <Avatar>
+                    <AvatarImage src='' />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </Link>
+                <div>
+                  <Link className='font-semibold text-xs'></Link>
+                  {/* <span className='text-gray-600 text-sm'>Bio here...</span> */}
+                </div>
+              </div>
+
+                 <Dialog>
+                <DialogTrigger asChild>
+                  <MoreHorizontal className='cursor-pointer' />
+                </DialogTrigger>
+                <DialogContent className="flex flex-col items-center text-sm text-center">
+                  <div className='cursor-pointer w-full text-[#ED4956] font-bold'>
+                    Unfollow
+                  </div>
+                  <div className='cursor-pointer w-full'>
+                    Add to favorites
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <hr />
+
+             <div className='flex-1 overflow-y-auto max-h-96 p-4'>
+              {/* {
+                comment.map((comment) => <Comment key={comment._id} comment={comment} />)
+              } */}
+            </div>
+            <div className='p-4'>
+              <div className='flex items-center gap-2'>
+                <input type="text" value={text} placeholder='Add a comment...' className='w-full outline-none border text-sm border-gray-300 p-2 rounded' />
+                <Button disabled={!text.trim()}  variant="outline">Send</Button>
+              </div>
+            </div>
+          </div>
+      
       </DialogContent>
     </Dialog>
   )
